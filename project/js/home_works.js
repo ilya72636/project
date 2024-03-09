7d67574aa88e63b7c689c0d021086365c113ece4
ilya72636 <hohulinila380@gmail.com>
2024-03-09 10:51:45 +0600

    1

---
+++ project/js/home_works.js
@@ -0,0 +1,42 @@
+const gmailInput = document.querySelector('#gmail_input')
+const gmailButton = document.querySelector('#gmail_button')
+const gmailSpan = document.querySelector('#gmail_result')
+
+const gmailRegExp =/^[a-zA-Z0-9._%+-]+@gmail\.com$/
+
+gmailButton.addEventListener('click', ()=>{
+    if (gmailRegExp.test(gmailInput.value)) {
+        gmailSpan.innerHTML='Валидный Gmail адрес'
+        gmailSpan.style.color='green'
+    }else{
+        gmailSpan.innerHTML='Невалидный Gmail адрес'
+        gmailSpan.style.color='red'
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const parentBlock = document.querySelector('.parent_block')
+    const childBlock = document.querySelector('.child_block')
+
+    let currentPosition = 0
+
+    function moveRight() {
+        if (currentPosition < parentBlock.offsetWidth - childBlock.offsetWidth) {
+            currentPosition += 5; 
+            childBlock.style.left = `${currentPosition}px`
+            setTimeout(moveRight, 16)
+        }
+        
+    }
+    moveRight();
+});
---
+++ project/js/lesson.js
@@ -0,0 +1,15 @@
+const phoneInput = document.querySelector('#phone_input')
+const phoneButton = document.querySelector('#phone_button')
+const phoneSpan = document.querySelector('#phone_result')
+
+const regExp = /^\+996 [2579]\d{2} \d{2}-\d{2}-\d{2}$/
+
+phoneButton.addEventListener('click', () => {
+    if (regExp.test(phoneInput.value)) {
+        phoneSpan.innerHTML = 'ok';
+        phoneSpan.style.color = 'green'; 
+    } else {
+        phoneSpan.innerHTML = 'not ok';
+        phoneSpan.style.color = 'red';
+    }
+});
